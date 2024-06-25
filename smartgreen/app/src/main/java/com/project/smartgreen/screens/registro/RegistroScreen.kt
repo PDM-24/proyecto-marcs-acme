5b059cd29e41d13da521200ed865dcdfc713b200
Cbug18 <00006422@uca.edu.sv>
2024-06-25 05:34:07 -0600

    smartgreen last versiobn

--- smartgreen/app/src/main/java/com/project/smartgreen/screens/registro/RegistroScreen.kt
+++ smartgreen/app/src/main/java/com/project/smartgreen/screens/registro/RegistroScreen.kt
@@ -1,10 +1,13 @@
 package com.project.smartgreen.ui.components
 
 import android.Manifest
+import android.app.DatePickerDialog
 import android.content.Context
 import android.location.Location
 import android.location.LocationManager
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -16,7 +19,7 @@ import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import com.project.smartgreen.data.model.Comment
 import com.project.smartgreen.ui.viewmodel.MainViewModel
-import kotlinx.coroutines.launch
+import java.util.*
 
 @Composable
 fun RegistroScreen(navController: NavHostController, viewModel: MainViewModel) {
@@ -90,7 +93,25 @@ fun RegistroScreen(navController: NavHostController, viewModel: MainViewModel) {
             value = observationDateTime,
             onValueChange = { observationDateTime = it },
             label = { Text("Fecha de Observación") },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            readOnly = true,
+            trailingIcon = {
+                IconButton(onClick = {
+                    val calendar = Calendar.getInstance()
+                    val datePickerDialog = DatePickerDialog(
+                        context,
+                        { _, year, month, dayOfMonth ->
+                            observationDateTime = "$year-${month + 1}-$dayOfMonth"
+                        },
+                        calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)
+                    )
+                    datePickerDialog.show()
+                }) {
+                    Icon(Icons.Default.DateRange, contentDescription = "Select Date")
+                }
+            }
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
